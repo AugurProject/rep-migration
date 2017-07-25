1eb20a9be6dd716a0993b0b1bbfbd39b6da09cc0
Jack Peterson <jack@tinybike.net>
2017-07-24 17:52:51 -0700

    Tweaked deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,9 +9,12 @@ const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 const RepTokenContract = web3.eth.contract(RepToken.abi);
 
+const networkID = process.env.EXPECTED_NETWORK_ID;
+
 RepTokenContract.new([LEGACY_REP_CONTRACT_ADDRESS], { data: RepToken.unlinked_binary, from: process.env.SENDER, gas: 4000000 }, (err, contractInstance) => {
-  if (contractInstance.address && process.env.EXPECTED_NETWORK_ID) {
-    RepToken.networks[process.env.EXPECTED_NETWORK_ID].address = contractInstance.address;
+  if (contractInstance.address && networkID) {
+    if (!RepToken.networks[networkID]) RepToken.networks[networkID] = {};
+    RepToken.networks[networkID].address = contractInstance.address;
     fs.writeFile(__dirname + "/../build/contracts/RepToken.json", JSON.stringify(RepToken, null, 2), (err) => {
       if (err) console.error(err);
       process.exit(0);
