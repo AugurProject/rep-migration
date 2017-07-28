7b0477581b458516ec233930e50dc604ec7db400
Jack Peterson <jack@tinybike.net>
2017-07-28 09:57:11 -0700

    Removed unused imports

--- contracts/RepToken.sol
+++ contracts/RepToken.sol
@@ -1,8 +1,6 @@
 pragma solidity ^0.4.11;
 
-import 'zeppelin-solidity/contracts/token/StandardToken.sol';
 import 'zeppelin-solidity/contracts/token/PausableToken.sol';
-import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 import 'zeppelin-solidity/contracts/token/ERC20Basic.sol';
 
 
